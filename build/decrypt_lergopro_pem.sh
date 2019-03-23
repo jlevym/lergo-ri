e0e7d4a4443c781fb602a6e902e116e148939c7f
Jeffrey <4lergo@gmail.com>
2019-03-23 21:34:31 +0200

    output the pem in the file

--- build/decrypt_lergopro_pem.sh
+++ build/decrypt_lergopro_pem.sh
@@ -2,3 +2,6 @@ echo 'now inside decrypt_lergopro_pem.sh'
 echo 'TESTME_KEY with -md md5: ' $TESTME_KEY
 
 openssl aes-256-cbc -d -a -in build/lergopro.pem.enc -out conf/dev/lergopro.pem -md md5 -pass pass:$TESTME_KEY
+
+echo 'the lergopro pem is: '
+cat conf/dev/lergopro.pem
