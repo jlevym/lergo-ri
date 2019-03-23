6937c5ce3188ae7c51cbe12ab264ec3d58c75f8c
Jeffrey <4lergo@gmail.com>
2019-03-23 21:23:03 +0200

    paste new lergopro.pem.enc after running container in -it mode

--- build/decrypt_lergopro_pem.sh
+++ build/decrypt_lergopro_pem.sh
@@ -1,4 +1,4 @@
-
+echo 'now inside decrypt_lergopro_pem.sh'
 echo 'TESTME_KEY with -md md5: ' $TESTME_KEY
 
 openssl aes-256-cbc -d -a -in build/lergopro.pem.enc -out conf/dev/lergopro.pem -md md5 -pass pass:$TESTME_KEY
