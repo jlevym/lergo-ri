a6f24caea66c54f288d9273cafa6ba93d404c9ad
Jeffrey <4lergo@gmail.com>
2019-03-23 18:24:42 +0200

    correcting decrypt_lergopro_pem file

--- build/decrypt_lergopro_pem.sh
+++ build/decrypt_lergopro_pem.sh
@@ -1,5 +1,4 @@
-if [ "$1" != "" ];then
-    TESTME_KEY="$1"
-fi
+
+echo 'TESTME_KEY: ' $TESTME_KEY
 
 openssl aes-256-cbc -d -a -in build/lergopro.pem.enc -out conf/dev/lergopro.pem -pass pass:$TESTME_KEY
