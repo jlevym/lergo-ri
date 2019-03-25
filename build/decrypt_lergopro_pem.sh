d19cbd271fd640e6040166f85b843891e1815b2a
Jeffrey <4lergo@gmail.com>
2019-03-25 20:25:32 +0200

    removing unecessary code

--- build/decrypt_lergopro_pem.sh
+++ build/decrypt_lergopro_pem.sh
@@ -1,7 +1,5 @@
 echo 'now inside decrypt_lergopro_pem.sh'
-echo 'TESTME_KEY with -md md5: ' $TESTME_KEY
+# echo 'TESTME_KEY with -md md5: ' $TESTME_KEY
 
 openssl aes-256-cbc -d -a -in build/lergopro.pem.enc -out conf/dev/lergopro.pem -md md5 -pass pass:$TESTME_KEY
 
-echo 'the lergopro pem is: '
-cat conf/dev/lergopro.pem
