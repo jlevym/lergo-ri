029a4ec238260a7ebf34061c5d06dbcb1e1b3cdb
Jeffrey <4lergo@gmail.com>
2019-04-10 07:54:01 +0300

    using env variables

--- build/vagrant/synced_folder/tasks/send_mail.js
+++ build/vagrant/synced_folder/tasks/send_mail.js
@@ -1,11 +1,12 @@
 var nodemailer = require('nodemailer');
 var ses = require('nodemailer-ses-transport');
 
-console.log( 'accessKeyId:', $accessKeyId, 'and AWS_ACCESS_KEY_ID', $AWS_ACCESS_KEY_ID );
+var accessKeyId = process.env.accessKeyId;
+var secretAccessKey = process.env.secretAccessKey;
 // the aws accessKey and secretAccessKey are from lergopro
 var transporter = nodemailer.createTransport(ses({
-    accessKeyId: $accessKeyId,
-    secretAccessKey: $secretAccessKey,
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
     region : "eu-west-1"
 
 }));
