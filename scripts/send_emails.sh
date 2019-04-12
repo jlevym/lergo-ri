8304234600ee3fafc7d7fb7074b0cd686db80ba5
Jeffrey <4lergo@gmail.com>
2019-04-12 09:50:54 +0300

    fixing BUILD_NUMBER

--- scripts/send_emails.sh
+++ scripts/send_emails.sh
@@ -12,7 +12,7 @@ export CI_TIMESTAMP=${CI_STRING_TIME:-local-build-id}
 
 echo get the build number from build-tracker
 aws s3 cp s3://lergopro-backups/artifacts/build-number-tracker/build-tracker.txt build-tracker.txt
-CURRENT_NUMBER=`cat build-tracker.txt`
+export BUILD_NUMBER=`cat build-tracker.txt`
 
 # echo 'get the build number from build_number.txt'
 # export BUILD_NUMBER=$(cat /tmp_build/build_number.txt);
